8ea279f480c58cd937e0b64a19919fcebad34f2c
Mariana Abdias Gonçalves <98291335+m-abdias@users.noreply.github.com>
2022-10-28 14:50:38 -0300

    Projeto Devagram Node JS com MongoDB

---
+++ devagram-node-nextjs/pages/api/login.ts
@@ -0,0 +1,14 @@
+import next from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    const { login, senha } = req.body
+
+    if (login === 'admin@admin.com' && senha === 'Admin@123') {
+      return res.status(200).json({ msg: 'Usuário autenticado com Sucesso!' })
+    }
+    return res.status(400).json({ erro: 'Usuário ou senha não encontrado.' })
+  }
+  return res.status(405).json({ erro: 'Método informado não é válido' })
+}
