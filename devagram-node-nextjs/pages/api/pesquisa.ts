ca75e800a815add09406c2b3b992cfa74cc0fe57
Mariana Abdias Gonçalves <98291335+m-abdias@users.noreply.github.com>
2022-10-31 17:25:29 -0300

    Api Like / Deslike

--- devagram-node-nextjs/pages/api/pesquisa.ts
+++ devagram-node-nextjs/pages/api/pesquisa.ts
@@ -11,48 +11,52 @@ const pesquisaEndpoint = async (
 ) => {
   try {
     if (req.method === 'GET') {
-      // if (req?.query?.id) {
-      const usuarioEncontrado = await UsuarioModel.findById(req?.query?.id)
-      // if (!usuarioEncontrado) {
-      //     return res.status(400).json({ erro: 'Usuario nao encontrado' });
-      // }
+      if (req?.query?.id) {
+        const usuarioEncontrado = await UsuarioModel.findById(req?.query?.id)
+        if (!usuarioEncontrado) {
+          return res.status(400).json({ erro: 'Usuario nao encontrado' })
+        }
 
-      //     const user = {
-      //         senha: null,
-      //         segueEsseUsuario: false,
-      //         nome: usuarioEncontrado.nome,
-      //         email: usuarioEncontrado.email,
-      //         _id: usuarioEncontrado._id,
-      //         avatar: usuarioEncontrado.avatar,
-      //         seguidores: usuarioEncontrado.seguidores,
-      //         seguindo: usuarioEncontrado.seguindo,
-      //         publicacoes: usuarioEncontrado.publicacoes,
-      //     } as any;
+        //     const user = {
+        //         senha: null,
+        //         segueEsseUsuario: false,
+        //         nome: usuarioEncontrado.nome,
+        //         email: usuarioEncontrado.email,
+        //         _id: usuarioEncontrado._id,
+        //         avatar: usuarioEncontrado.avatar,
+        //         seguidores: usuarioEncontrado.seguidores,
+        //         seguindo: usuarioEncontrado.seguindo,
+        //         publicacoes: usuarioEncontrado.publicacoes,
+        //     } as any;
 
-      //     const segueEsseUsuario = await SeguidorModel.find({ usuarioId: req?.query?.userId, usuarioSeguidoId: usuarioEncontrado._id });
-      //     if (segueEsseUsuario && segueEsseUsuario.length > 0) {
-      //         user.segueEsseUsuario = true;
-      //     }
-      //     return res.status(200).json(user);
-      // } else {
-      const { filtro } = req.query
-      if (!filtro || filtro.length < 2) {
-        return res
-          .status(400)
-          .json({ erro: 'Favor informar pelo menos 2 caracteres para a busca' })
+        //     const segueEsseUsuario = await SeguidorModel.find({ usuarioId: req?.query?.userId, usuarioSeguidoId: usuarioEncontrado._id });
+        //     if (segueEsseUsuario && segueEsseUsuario.length > 0) {
+        //         user.segueEsseUsuario = true;
+        //     }
+            return res.status(200).json(usuarioEncontrado);
+      } else {
+        const { filtro } = req.query
+        if (!filtro || filtro.length < 2) {
+          return res
+            .status(400)
+            .json({
+              erro: 'Favor informar pelo menos 2 caracteres para a busca'
+            })
+        }
+
+        const usuariosEncontrados = await UsuarioModel.find({
+          $or: [
+            { nome: { $regex: filtro, $options: 'i' } },
+            { email: { $regex: filtro, $options: 'i' } }
+          ]
+        })
+
+        //     usuariosEncontrados.forEach(userFound => {
+        //         userFound.senha = null
+        //     });
+
+        return res.status(200).json(usuariosEncontrados)
       }
-
-      const usuariosEncontrados = await UsuarioModel.find({
-        $or: [{nome: { $regex: filtro, $options: 'i' }},
-          {email: { $regex: filtro, $options: 'i' }}]
-      })
-
-      //     usuariosEncontrados.forEach(userFound => {
-      //         userFound.senha = null
-      //     });
-
-      return res.status(200).json(usuariosEncontrados)
-      // }
     }
     return res.status(405).json({ erro: 'Metodo informado nao e valido' })
   } catch (e) {
